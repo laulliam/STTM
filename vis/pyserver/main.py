e54cc464d2f198fb1d21a016af94f0691cbb4150
Laulliam <laulliam@outlook.com>
2022-12-27 18:07:47 +0800

    feat: request all topics data

--- vis/pyserver/main.py
+++ vis/pyserver/main.py
@@ -10,15 +10,16 @@ CORS(app, resources=r'/*')	# 注册CORS, "/*" 允许访问所有api
 
 @app.route('/topic')
 def index():
-    topic_id = int(request.args.get("id")) | 0
     time_slice_num = int(request.args.get("t")) | 0
     model_filepath = "../../model/chengdutaxi_process/"
     model_filename = 'model_20140803_full'
     ldaseq = DtmModel.load(os.path.join(model_filepath, model_filename))
-    topic = ldaseq.show_topic(topicid=topic_id, time=time_slice_num, topn=50)
-    topic = [[part[0], part[1], GirdID2LngLat(part[1].split('-')[0]), GirdID2LngLat(part[1].split('-')[1]) ] for part in topic]
-
-    return jsonify(code=200, data=topic)
+    data = []
+    for i in range(18):
+        topic = ldaseq.show_topic(topicid=i, time=time_slice_num, topn=50)
+        topic = [[part[0], part[1], GirdID2LngLat(part[1].split('-')[0]), GirdID2LngLat(part[1].split('-')[1]) ] for part in topic]
+        data.append({"id":i,"data":topic})
+    return jsonify(code=200, data=data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
